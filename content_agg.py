8c615716b04052b8464773c6f2a888604a6384a3
real-4w <real_4w@protonmail.com>
2021-11-05 15:31:21 +1300

    WIP

--- content_agg.py
+++ content_agg.py
@@ -1,3 +1,5 @@
+#https://codingkaiser.blog/2021/10/30/create-a-content-aggregator-with-python/
+
 from abc import ABC, abstractmethod
 import praw
 import os
@@ -12,3 +14,37 @@ class Source(ABC):
   def fetch(self):
     pass
 
+CLIENT_ID = os.environ.get('REDDIT_CLIENT_ID')
+CLIENT_SECRET = os.environ.get('REDDIT_CLIENT_SECRET')
+
+class RedditSource(Source):
+
+  def connect(self):
+    self.reddit_con = praw.Reddit(client_id=CLIENT_ID,
+                      client_secret=CLIENT_SECRET,
+                      grant_type_access='client_credentials',
+                      user_agent='script/1.0')
+    return self.reddit_con
+
+  def fetch(self):
+    pass
+
+class RedditHotProgramming(RedditSource):
+
+  def __init__(self) -> None:
+    self.reddit_con = super().connect()
+    self.hot_submissions = []
+
+  def fetch(self, limit: int):
+    self.hot_submissions = self.reddit_con.subreddit('programming').hot(limit=limit)
+
+  def __repr__(self):
+    urls = []
+    for submission in self.hot_submissions:
+      urls.append(vars(submission)['url'])
+    return '\n'.join(urls)
+
+if __name__ == '__main__':
+  reddit_top_programming = RedditHotProgramming()
+  reddit_top_programming.fetch(limit=10)
+  print(reddit_top_programming)
